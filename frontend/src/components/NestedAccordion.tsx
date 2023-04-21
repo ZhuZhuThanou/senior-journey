5dfe1c83694c18769e90ed286ed5d4cce573fe95
ZhuZhuThanou <blogicalbmath@gmail.com>
2023-04-20 21:54:56 -0400

    update task data model

--- frontend/src/components/NestedAccordion.tsx
+++ frontend/src/components/NestedAccordion.tsx
@@ -37,7 +37,7 @@ const AccordionInside: React.FC<Task> = (task) => {
                 className="bg-gray-400 text-black px-4 py-2 cursor-pointer flex justify-between items-center"
                 onClick={() => setIsOpenInner(!isOpenInner)}
             >
-                <span>title</span>
+                <span>{task.title}</span>
                 <strong>
                     {isOpenInner ? '-' : '+'}
                 </strong>
